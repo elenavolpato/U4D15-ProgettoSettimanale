9582dd556dacc0f5943150c21a30fcd63967fa00
raposinha <elena@raposinha.dev>
2026-03-27 10:25:13 +0100

    adds books and magazine entities

--- src/main/java/elena/entities/Book.java
+++ src/main/java/elena/entities/Book.java
@@ -1,4 +1,33 @@
 package elena.entities;
 
-public class Book {
+import elena.enumerated.BookGenre;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="books")
+
+public class Book extends LibraryItem{
+    private String author;
+    private BookGenre genre;
+
+    public Book(){};
+
+    public Book(String ISBN, int year, int pages, String author, BookGenre genre){
+        super(ISBN, year,pages);
+        this.author = author;
+        this.genre = BookGenre.ROMANCE;
+    }
+
+    public BookGenre getGenre() {        return genre;    }
+    public void setGenre(BookGenre genre) {        this.genre = genre;    }
+    public String getAuthor() {        return author;    }
+    public void setAuthor(String author) {        this.author = author;    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", genre=" + genre +
+                '}';
+    }
 }
--- src/main/java/elena/entities/Magazine.java
+++ src/main/java/elena/entities/Magazine.java
@@ -1,4 +1,30 @@
 package elena.entities;
 
-public class Magazine {
+import elena.enumerated.Periodicity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="magazine")
+
+public class Magazine extends LibraryItem{
+
+    private Periodicity periodicity;
+
+    public Magazine(){};
+
+    public Magazine(String ISBN, int year, int pages, Periodicity periodicity){
+        super(ISBN, year,pages);
+        this.periodicity = Periodicity.WEEKLY;
+    }
+
+    public Periodicity getPeriodicity() {        return periodicity;    }
+
+    public void setPeriodicity(Periodicity periodicity) {        this.periodicity = periodicity;    }
+
+    @Override
+    public String toString() {
+        return "Magazine{" +
+                "periodicity=" + periodicity +
+                '}';
+    }
 }
